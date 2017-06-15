660cf854044fe6cdb63108dea091ab309c4fec29
Tinny666 <connortinsley@gmail.com>
2017-06-15 14:06:42 +0100

    Add files via upload

---
+++ playerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public float topSpeed = 10f;    //This is used as a variable to determine the speed the player move at.
+    bool facingRight = true;        //This is a Boolean that will act as a trigger to understand if the player is facing right, which is recognized as true if they go right.
+    Animator anim;                  //This is used to control the animation system that will be in use.
+
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 250f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Grounded", false);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+     }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Grounded", grounded);
+
+        anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+
+        float move = Input.GetAxis("Horizontal");
+        Debug.Log(move);
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+      }
+
+    void Flip()
+     {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+       transform.localScale = theScale;
+    }
+}
